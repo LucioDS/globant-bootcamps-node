583dfc1a6d6f66517d61c875b35634438a2f1be0
luciods1 <luciodesimone@yahoo.com.ar>
2021-05-29 18:23:28 -0300

    remove URL and unused logging when server starts

--- exercises/chapter_5/src/api-server/server.js
+++ exercises/chapter_5/src/api-server/server.js
@@ -1,22 +1,19 @@
-
 /*! Copyright Globant. All rights reserved. */
-'use strict';
+"use strict";
 
 // This will be our application entry. We'll setup our server here.
-const http = require('http');
-const app = require('./core/app'); // The express app we just created
-const config = require('./core/config');
+const http = require("http");
+const app = require("./core/app"); // The express app we just created
+const config = require("./core/config");
 
-app.set('port', config.port);
+app.set("port", config.port);
 
 const server = http.createServer(app);
 
-server.listen(config.port, function() {
-    const url = `${config.hostname}:${config.port}${config.basePath}`;
-    const line = (new Array(80)).join('_');
+server.listen(config.port, function () {
+  const line = new Array(80).join("_");
 
-    console.info(line);
-    console.info(`WebAPI server running at ${url}`);
-    console.info(`  >>> start: ${new Date()}`);
-    console.info(line + '\n');
+  console.info(line);
+  console.info(`start: ${new Date()}`);
+  console.info(line + "\n");
 });
